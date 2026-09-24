29428d6fccca190efa8a453517897b60bd0eb628
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-23 23:45:24 -0400

    Cache premium recurring calendar visual library

--- pages/sw.js
+++ pages/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "abvm-grade2-parent-companion-v14";
+const CACHE = "abvm-grade2-parent-companion-v15";
 const SHELL = [
   "./",
   "./index.html",
@@ -19,6 +19,7 @@ const SHELL = [
   "./assets/lunch-thursday.webp",
   "./assets/lunch-friday.webp",
   "./assets/calendar/picture-day.svg",
+  "./assets/calendar/picture-day-2.svg",
   "./assets/calendar/mass-1.svg",
   "./assets/calendar/mass-2.svg",
   "./assets/calendar/gym-1.svg",
